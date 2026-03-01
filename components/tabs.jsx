3b6274dec60d204ec3c994a759ad04d76398ed33
jyanza63 <jyanza63@gmail.com>
2026-03-01 17:48:58 -0500

    Add Tabs component for navigation
    Signed-off-by: jyanza63 <jyanza63@gmail.com>

---
+++ components/tabs.jsx
@@ -0,0 +1,9 @@
+export default function Tabs() {
+  return (
+    <div className="tabs">
+      <button className="tab active">Account Settings</button>
+      <button className="tab">Company Settings</button>
+      <button className="tab">Notifications</button>
+    </div>
+  );
+}
